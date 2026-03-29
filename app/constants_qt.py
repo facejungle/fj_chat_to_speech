eb570ab3837ad8b7a0438dcc477b4b67e31782fc
Andrey F <sqin00@gmail.com>
2026-03-29 07:34:52 +0300

    Changed constants with Qt color set

--- app/constants_qt.py
+++ app/constants_qt.py
@@ -1,12 +1,26 @@
+from typing import Literal
+
 from PyQt6.QtGui import QColor
 
-COLORS = {
-    "TRANSPARENT_BLACK_Q": QColor(0, 0, 0, 150),
-    "TRANSPARENT_BLACK": "rgba(0, 0, 0, 150)",
-    "TRANSPARENT_BLACK2_Q": QColor(0, 0, 0, 200),
-    "TRANSPARENT_BLACK2": "rgba(0, 0, 0, 200)",
-    "WHITE_Q": QColor("#ffffff"),
-    "WHITE": "#ffffff",
-    "GRAY_Q": QColor("#444444"),
-    "GRAY": "#444444",
+type ColorsLiteral = Literal[
+    "BLACK", "WHITE", "GRAY", "YELLOW", "GREEN", "BLUE", "ORANGE"
+]
+COLORS_RGBA: dict[ColorsLiteral, QColor] = {
+    "BLACK": QColor(0, 0, 0, 200),
+    "WHITE": QColor(255, 255, 255, 220),
+    "GRAY": QColor(68, 68, 68, 220),
+    "YELLOW": QColor(92, 91, 26, 220),
+    "GREEN": QColor(51, 94, 22, 220),
+    "BLUE": QColor(17, 27, 54, 220),
+    "ORANGE": QColor(69, 19, 19, 220),
+}
+
+COLORS_SOLID: dict[ColorsLiteral, QColor] = {
+    "BLACK": QColor(0, 0, 0, 220),
+    "WHITE": QColor(255, 255, 255, 255),
+    "GRAY": QColor(68, 68, 68, 255),
+    "YELLOW": QColor(92, 91, 26, 255),
+    "GREEN": QColor(51, 94, 22, 255),
+    "BLUE": QColor(17, 27, 54, 255),
+    "ORANGE": QColor(69, 19, 19, 255),
 }
